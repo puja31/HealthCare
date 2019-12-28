2a4e70f194fa0382891b9352a118d7952ae535f5
Pujitha Nadimpalli <pujithanvarma@gmail.com>
2019-12-28 06:05:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/healthcare/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
+    }
+}
